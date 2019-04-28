f8f4e22e9d7e6dd32c4dbe76d3f0836f8058b7cc
William <akyei908@gmail.com>
2019-04-28 02:24:36 +0000

    more stuff added

--- app/src/main/java/com/example/repsyche/SignUpPage.java
+++ app/src/main/java/com/example/repsyche/SignUpPage.java
@@ -2,12 +2,9 @@ package com.example.repsyche;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
@@ -29,6 +26,26 @@ public class SignUpPage extends AppCompatActivity {
         Intent n = new Intent(this, LoginPage.class);
         startActivity(n);
 
+    }
+
+    public void signUp_onclick(View view){
+        EditText firstname = (EditText) findViewById(R.id.FirstName);
+        String fname_content = firstname.getText().toString();
+
+        EditText lasttname = (EditText) findViewById(R.id.LastName);
+        String lname_content = lasttname.getText().toString();
+
+        EditText username = (EditText) findViewById(R.id.SignUp_Username);
+        String username_content = username.getText().toString();
+
+        EditText email = (EditText) findViewById(R.id.SignUp_Email);
+        String email_content = username.getText().toString();
+
+        EditText password = (EditText) findViewById(R.id.SignUp_Password);
+        String password_content = password.getText().toString();
+
+        EditText confirm_password = (EditText) findViewById(R.id.SignUp_ConfirmPassword);
+        String confirmpassword_content = confirm_password.getText().toString();
     }
 
     public native boolean newUser(String userName, String email);
